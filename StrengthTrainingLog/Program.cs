b956bf2ce650ead1a8ca1396806259b245faafcc
Scott DeMers <scott.demers@outlook.com>
2016-10-17 10:35:19 -0600

    Initial Committ

---
+++ StrengthTrainingLog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthTrainingLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrainingLog log = new TrainingLog();
+            log.AddSession("24 hr fitness kettlebell session");
+            log.AddSession("Home Yoga Session in da basement");
+        }
+    }
+}
---
+++ StrengthTrainingLog/TrainingLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthTrainingLog
+{
+    class TrainingLog
+    {   
+        //Acesss modifiers
+        ///public
+        ///private - default - only accessible within the class
+
+
+        ////Constructor - a method definition the same name as the class. Allows us to take control of the initialization of an object
+        //is invoked to when 'new' keyword is is used to create a new TrainingLog
+        //ctro tab tab is code snippet
+        public TrainingLog()
+        {
+            sessions = new List<string>();
+        }
+        
+        //session is scoped to this method
+        public void AddSession(string session)
+        {
+            sessions.Add(session);
+
+        }
+
+        public List<string> sessions;
+    }
+}
